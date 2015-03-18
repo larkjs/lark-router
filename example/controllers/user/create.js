9619e70c1a7e4867e89e9fb45829d16d7895366e
mdemo <mengdesen09@qq.com>
2015-03-18 11:46:49 +0800

    Update lark-router

--- example/controllers/user/create.js
+++ example/controllers/user/create.js
@@ -1,10 +1,11 @@
 /**
  * Created by mdemo on 14/11/13.
  */
-module.exports = function(router){
-  router.get('/', function *(next){
-    this.body = 'Hello Create';
-    yield next;
-  });
-  return router;
+module.exports = function (router) {
+    router.get('/', function *(next) {
+        console.dir(this.mountPath);
+        this.body = 'Hello Create';
+        yield next;
+    });
+    return router;
 };
