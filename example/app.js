51d42a0b1be19f9a41119b760b11146c44165e0d
Sun Haohao <viringbells@q..com>
2017-04-05 19:58:06 +0800

    re-dev, remove router.load

---
+++ example/app.js
@@ -0,0 +1,59 @@
+/**
+ * Test for lark router
+ **/
+'use strict';
+process.mainModule = module;
+
+const Koa     = require('koa');
+const Router  = require('..');
+
+const router = new Router();
+
+router.get('/home/:name', async (ctx, next) => {
+    ctx.body = `Welcome home, ${ctx.params.name}`;
+    await next();
+});
+router.post('/home/:name/:action', async (ctx, next) => {
+    ctx.body = `You have posted an action "${ctx.params.action}" to ${ctx.params.name}`;
+    await next();
+});
+
+const router2 = new Router();
+router2.get('/foo/:bar', async (ctx, next) => {
+    let message = ctx.params.bar;
+    if (ctx.params.prefix) {
+        message = ctx.params.prefix + ':' + message;
+    }
+    ctx.body = `Foo - Bar = ${message}`;
+    await next();
+});
+
+router.get('/2', router2);
+router.get('/2/:prefix', router2);
+
+router.get('/welcome/:name', async (ctx, next) => {
+    ctx.body = `Welcome, ${ctx.params.name} `;
+    await next();
+});
+router.routed('/welcome/*', async (ctx) => {
+    ctx.body += '[Routed]';
+});
+router.other('/welcome/*', async (ctx, next) => {
+    ctx.body = 'Not matched ';
+    await next();
+});
+
+router.all('/welcome/*', async (ctx) => {
+    ctx.body += '[All]';
+});
+
+router.get(/^\/regexp\/(\d+)$/, async (ctx, next) => {
+    ctx.body = `input is ${ctx.params['0']}`;
+    await next();
+});
+
+const app = new Koa();
+
+app.use(router.routes());
+module.exports = app.listen(3000);
+
---
+++ test/index.js
@@ -0,0 +1,78 @@
+/**
+ * Test for lark-router
+ **/
+'use strict';
+
+const agent = require('supertest');
+
+const Router = require('..');
+const app = require('../example/app');
+
+const request = agent(app);
+
+describe('lark router app', () => {
+    it('should response 404 for not found url', (done) => {
+        request.get('/404/not/found')
+            .expect(404)
+            .end(done);
+    });
+
+    it('should response 200 for get /home/haohao', (done) => {
+        request.get('/home/haohao')
+            .expect(200)
+            .expect('Welcome home, haohao', done);
+    });
+
+    it('should response 200 for post /home/haohao/logout', (done) => {
+        request.post('/home/haohao/logout')
+            .expect(200)
+            .expect('You have posted an action "logout" to haohao', done);
+    });
+
+    it('should response 200 for get /2/foo/bar', (done) => {
+        request.get('/2/foo/bar')
+            .expect(200)
+            .expect('Foo - Bar = bar', done);
+    });
+
+    it('should response 200 for get /2/hello/foo/bar', (done) => {
+        request.get('/2/hello/foo/bar')
+            .expect(200)
+            .expect('Foo - Bar = hello:bar', done);
+    });
+
+    it('should response 200 for get /welcome/haohao', (done) => {
+        request.get('/welcome/haohao')
+            .expect(200)
+            .expect('Welcome, haohao [Routed][All]', done);
+    });
+
+    it('should response 200 for post /welcome/haohao', (done) => {
+        request.post('/welcome/haohao')
+            .expect(200)
+            .expect('Not matched [All]', done);
+    });
+
+    it('should resposne 200 for get /regexp/1', (done) => {
+        request.get('/regexp/1')
+            .expect(200)
+            .expect('input is 1', done);
+    });
+
+});
+
+describe('start a router with undefined method', () => {
+    it('should throw error', (done) => {
+        const router = new Router();
+        let error = {};
+        try {
+            router.test('/test', async (ctx, async) => {
+            });
+        }
+        catch (e) {
+            error = e;
+        }
+        error.should.be.an.instanceof(Error);
+        done();
+    });
+});
